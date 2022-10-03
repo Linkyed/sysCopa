78757e6032d284da2754c99c4c708c38e7dfac9d
Nalbertsan <nalbert1000@gmail.com>
2022-10-02 23:26:26 -0300

    Main tambem

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,19 +1,13 @@
 package application;
-
-//import javafx.application.Application;
-//import javafx.stage.Stage;
 import model.*;
-//import javafx.scene.Scene;
-//import javafx.scene.layout.BorderPane;
 
+/**
+ * Classe Main para a execulsão do codígo.
+ * @author Nalbert Santos Araujo
+ * @author Pedro Henrique
+ * **/
 public class Main {
-	/*
-	 * public void start(Stage primaryStage) { try { BorderPane root = new
-	 * BorderPane(); Scene scene = new Scene(root,400,400);
-	 * scene.getStylesheets().add(getClass().getResource("application.css").
-	 * toExternalForm()); primaryStage.setScene(scene); primaryStage.show(); }
-	 * catch(Exception e) { e.printStackTrace(); } }
-	 */
+	
 
 	public static void main(String[] args) {
 		// Variaveis para desiginar a escolha do usuario perante as ações que o progama
