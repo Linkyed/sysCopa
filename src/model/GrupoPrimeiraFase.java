62c3a382fea7f212069b30f968c31ee8135d7085
Nalbertsan <nalbert1000@gmail.com>
2022-10-13 22:39:31 -0300

    Partidas feitas automaticamente

--- src/model/GrupoPrimeiraFase.java
+++ src/model/GrupoPrimeiraFase.java
@@ -1,6 +1,8 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class GrupoPrimeiraFase {
@@ -16,29 +18,29 @@ public class GrupoPrimeiraFase {
 	public static boolean adicionarSelecao(String grupo, Selecao selecao) {
 		boolean addBoolean = SelecaoDAO.inserir(selecao);
 		if (addBoolean) {
-			if (grupo.equalsIgnoreCase("A") && grupoA.size() > 5) {
+			if (grupo.equalsIgnoreCase("A") && grupoA.size() < 5) {
 				grupoA.put(selecao, 0);
-			} else if (grupo.equalsIgnoreCase("B")&& grupoB.size() > 5) {
+			} else if (grupo.equalsIgnoreCase("B") && grupoB.size() < 5) {
 				grupoB.put(selecao, 0);
-			} else if (grupo.equalsIgnoreCase("C")&& grupoC.size() > 5) {
+			} else if (grupo.equalsIgnoreCase("C") && grupoC.size() < 5) {
 				grupoC.put(selecao, 0);
 
-			} else if (grupo.equalsIgnoreCase("D")&& grupoD.size() > 5) {
+			} else if (grupo.equalsIgnoreCase("D") && grupoD.size() < 5) {
 				grupoD.put(selecao, 0);
 
-			} else if (grupo.equalsIgnoreCase("E")&& grupoE.size() > 5) {
+			} else if (grupo.equalsIgnoreCase("E") && grupoE.size() < 5) {
 				grupoE.put(selecao, 0);
 
-			} else if (grupo.equalsIgnoreCase("F")&& grupoF.size() > 5) {
+			} else if (grupo.equalsIgnoreCase("F") && grupoF.size() < 5) {
 				grupoF.put(selecao, 0);
 
-			} else if (grupo.equalsIgnoreCase("G")&& grupoG.size() > 5) {
+			} else if (grupo.equalsIgnoreCase("G") && grupoG.size() < 5) {
 				grupoG.put(selecao, 0);
 
-			} else if (grupo.equalsIgnoreCase("H")&& grupoH.size() > 5) {
+			} else if (grupo.equalsIgnoreCase("H") && grupoH.size() < 5) {
 				grupoH.put(selecao, 0);
 
-			}else {
+			} else {
 				return false;
 			}
 			return true;
@@ -46,17 +48,44 @@ public class GrupoPrimeiraFase {
 
 		return false;
 	}
-	
-	public static boolean organizadorPartidas() {
-		int totalSelecoes = grupoA.size() + grupoB.size() + grupoC.size()+ grupoD.size()+ grupoE.size()+ grupoF.size() + grupoG.size() + grupoH.size();
-		if(totalSelecoes == 32) {
-			for(int i = 0; i < 9; i++) {
-				
-			}
+
+	private static void organizadorGrupo(Map<Selecao, Integer> grupo) {
+		List<Selecao> selecoesGrupoList = new ArrayList<>();
+		for (Map.Entry<Selecao, Integer> selecaoEpontos : grupo.entrySet()) {
+			Selecao selecao = selecaoEpontos.getKey();
+			selecoesGrupoList.add(selecao);
+		}
+		for (int i = 1; i < 4; i++) {
+			Partida modeloPartida = new Partida(selecoesGrupoList.get(0), selecoesGrupoList.get(i));
+			PartidaDAO.inserir(modeloPartida);
+		}
+		for (int i = 2; i < 4; i++) {
+			Partida modeloPartida = new Partida(selecoesGrupoList.get(1), selecoesGrupoList.get(i));
+			PartidaDAO.inserir(modeloPartida);
+		}
+		for (int i = 3; i < 4; i++) {
+			Partida modeloPartida = new Partida(selecoesGrupoList.get(2), selecoesGrupoList.get(i));
+			PartidaDAO.inserir(modeloPartida);
+		}
+
+	}
+
+	public static boolean organizadorTodasPartidas() {
+		int totalSelecoes = grupoA.size() + grupoB.size() + grupoC.size() + grupoD.size() + grupoE.size()
+				+ grupoF.size() + grupoG.size() + grupoH.size();
+		if (totalSelecoes == 32) {
+			organizadorGrupo(grupoA);
+			organizadorGrupo(grupoB);
+			organizadorGrupo(grupoC);
+			organizadorGrupo(grupoD);
+			organizadorGrupo(grupoE);
+			organizadorGrupo(grupoF);
+			organizadorGrupo(grupoG);
+			organizadorGrupo(grupoH);
 			return true;
 		}
 		return false;
-		
+
 	}
 
 }
--- src/model/Teste.java
+++ src/model/Teste.java
@@ -3,15 +3,83 @@ package model;
 public class Teste {
 
 	public static void main(String[] args) {
-		Selecao selecao1 = new Selecao("Brasil");
-		Selecao selecao2 = new Selecao("Alemanha");
-		Selecao selecao3 = new Selecao("França");
-		Partida partida = new Partida(0, 0, null, null, selecao1, selecao2);
-		Partida partida2 = new Partida(0, 0, null, null, selecao1, selecao3);
-		PartidaDAO.inserir(partida);
-		PartidaDAO.inserir(partida2);
+		/
+		Selecao selecao1 = new Selecao("Qatar");
+		Selecao selecao2 = new Selecao("Ecuador");
+		Selecao selecao3 = new Selecao("Senegal");
+		Selecao selecao4 = new Selecao("Holanda");
+		Selecao selecao5 = new Selecao("Inglaterra");
+		Selecao selecao6 = new Selecao("Irã");
+		Selecao selecao7 = new Selecao("Estados Unidos");
+		Selecao selecao8 = new Selecao("Pais de Gales");
+		Selecao selecao9 = new Selecao("Argentina");
+		Selecao selecao10 = new Selecao("Árabia Saudita");
+		Selecao selecao11 = new Selecao("México");
+		Selecao selecao12 = new Selecao("Polônia");
+		Selecao selecao13 = new Selecao("França");
+		Selecao selecao14 = new Selecao("Australia");
+		Selecao selecao15 = new Selecao("Dinamarca");
+		Selecao selecao16 = new Selecao("Tunísia");
+		Selecao selecao17 = new Selecao("Espanha");
+		Selecao selecao18 = new Selecao("Costa Rica");
+		Selecao selecao19 = new Selecao("Alemanha");
+		Selecao selecao20 = new Selecao("Japão");
+		Selecao selecao21 = new Selecao("Bélgica");
+		Selecao selecao22 = new Selecao("Canadá");
+		Selecao selecao23 = new Selecao("Marrocos");
+		Selecao selecao24 = new Selecao("Croácia");
+		Selecao selecao25 = new Selecao("Brasil");
+		Selecao selecao26 = new Selecao("Sérvia");
+		Selecao selecao27 = new Selecao("Suíça");
+		Selecao selecao28 = new Selecao("Camarões");
+		Selecao selecao29 = new Selecao("Portugal");
+		Selecao selecao30 = new Selecao("Gana");
+		Selecao selecao31 = new Selecao("Uruguai");
+		Selecao selecao32 = new Selecao("Coreia do Sul");
+		
+		GrupoPrimeiraFase.adicionarSelecao("A", selecao1);
+		GrupoPrimeiraFase.adicionarSelecao("A", selecao2);
+		GrupoPrimeiraFase.adicionarSelecao("A", selecao3);
+		GrupoPrimeiraFase.adicionarSelecao("A", selecao4);
+		
+		GrupoPrimeiraFase.adicionarSelecao("B", selecao5);
+		GrupoPrimeiraFase.adicionarSelecao("B", selecao6);
+		GrupoPrimeiraFase.adicionarSelecao("B", selecao7);
+		GrupoPrimeiraFase.adicionarSelecao("B", selecao8);
+		
+		GrupoPrimeiraFase.adicionarSelecao("C", selecao9);
+		GrupoPrimeiraFase.adicionarSelecao("C", selecao10);
+		GrupoPrimeiraFase.adicionarSelecao("C", selecao11);
+		GrupoPrimeiraFase.adicionarSelecao("C", selecao12);
+		
+		GrupoPrimeiraFase.adicionarSelecao("D", selecao13);
+		GrupoPrimeiraFase.adicionarSelecao("D", selecao14);
+		GrupoPrimeiraFase.adicionarSelecao("D", selecao15);
+		GrupoPrimeiraFase.adicionarSelecao("D", selecao16);
+		
+		GrupoPrimeiraFase.adicionarSelecao("E", selecao17);
+		GrupoPrimeiraFase.adicionarSelecao("E", selecao18);
+		GrupoPrimeiraFase.adicionarSelecao("E", selecao19);
+		GrupoPrimeiraFase.adicionarSelecao("E", selecao20);
+		
+		GrupoPrimeiraFase.adicionarSelecao("F", selecao21);
+		GrupoPrimeiraFase.adicionarSelecao("F", selecao22);
+		GrupoPrimeiraFase.adicionarSelecao("F", selecao23);
+		GrupoPrimeiraFase.adicionarSelecao("F", selecao24);
+		
+		GrupoPrimeiraFase.adicionarSelecao("G", selecao25);
+		GrupoPrimeiraFase.adicionarSelecao("G", selecao26);
+		GrupoPrimeiraFase.adicionarSelecao("G", selecao27);
+		GrupoPrimeiraFase.adicionarSelecao("G", selecao28);
+		
+		GrupoPrimeiraFase.adicionarSelecao("H", selecao29);
+		GrupoPrimeiraFase.adicionarSelecao("H", selecao30);
+		GrupoPrimeiraFase.adicionarSelecao("H", selecao31);
+		GrupoPrimeiraFase.adicionarSelecao("H", selecao32);
+		
+		GrupoPrimeiraFase.organizadorTodasPartidas();
 		PartidaDAO.listar();
-
+		
 	}
 
 }
