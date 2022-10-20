9b98ac0c5748c072f97e448abe6ee064b4211a0f
Nalbertsan <nalbert1000@gmail.com>
2022-10-20 00:03:14 -0300

    Editar Gol, cart A, Cart V e Arbitro

--- src/model/Partida.java
+++ src/model/Partida.java
@@ -35,7 +35,6 @@ public class Partida {
 
 	}
 
-
 	public String getCodigoPartida() {
 		return codigoPartida;
 	}
@@ -146,80 +145,69 @@ public class Partida {
 		return resultadoSelecao;
 	}
 
-
 	public void setResultadoSelecao(Selecao resultadoSelecao) {
 		this.resultadoSelecao = resultadoSelecao;
 	}
-	
+
 	public boolean getStatus() {
 		return status;
 	}
-
 
 	public void setStatus(boolean status) {
 		this.status = status;
 	}
 
-
 	public int getHorarioM() {
 		return horarioM;
 	}
-
 
 	public void setHorarioM(int horarioM) {
 		this.horarioM = horarioM;
 	}
 
-
 	public List<Arbitro> getListaArbitro() {
 		return listaArbitro;
 	}
-
+	
+	public void setListaArbitro(List<Arbitro> arbitros) {
+		this.listaArbitro = arbitros;
+	}
 
 	public void setCartaoAmareloSelecao1(Map<Jogador, Integer> cartaoAmareloPartida) {
 		this.cartaoAmareloSelecao1 = cartaoAmareloPartida;
 	}
 
-
 	public void setCartaoVermelhoSelecao1(Map<Jogador, Integer> cartaoVermelhoPartida) {
 		this.cartaoVermelhoSelecao1 = cartaoVermelhoPartida;
 	}
-
 
 	public void setGolsMarcadosSelecao1(Map<Jogador, Integer> golsMarcaosPartida) {
 		GolsMarcadosSelecao1 = golsMarcaosPartida;
 	}
 
-
 	public Map<Jogador, Integer> getCartaoAmareloSelecao2() {
 		return cartaoAmareloSelecao2;
 	}
-
 
 	public void setCartaoAmareloSelecao2(Map<Jogador, Integer> cartaoAmareloSelecao2) {
 		this.cartaoAmareloSelecao2 = cartaoAmareloSelecao2;
 	}
 
-
 	public Map<Jogador, Integer> getCartaoVermelhoSelecao2() {
 		return cartaoVermelhoSelecao2;
 	}
-
 
 	public void setCartaoVermelhoSelecao2(Map<Jogador, Integer> cartaoVermelhoSelecao2) {
 		this.cartaoVermelhoSelecao2 = cartaoVermelhoSelecao2;
 	}
 
-
 	public Map<Jogador, Integer> getGolsMarcadosSelecao2() {
 		return GolsMarcadosSelecao2;
 	}
 
-
 	public void setGolsMarcadosSelecao2(Map<Jogador, Integer> golsMarcadosSelecao2) {
 		GolsMarcadosSelecao2 = golsMarcadosSelecao2;
 	}
-
 
 	@Override
 	public int hashCode() {
@@ -238,8 +226,5 @@ public class Partida {
 		return (Objects.equals(selecao1, other.selecao1) && Objects.equals(selecao2, other.selecao2))
 				|| (Objects.equals(selecao1, other.selecao2) && Objects.equals(selecao2, other.selecao1));
 	}
-
-
-	
 
 }
